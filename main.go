b843d60e78f55d5c052d0d860b7ea5bdda36ad1d
Adil Farooque <Adilfarq11@gmail.com>
2024-03-23 16:44:40 +0530

    Added the first handler

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello World!")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Start server on :4000")
+	if err := http.ListenAndServe(":4000", mux); err != nil {
+		log.Fatal(err)
+	}
+
 }
