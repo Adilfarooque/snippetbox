a29007e7a94c52565954afdc083945dc28f4e436
Adil Farooque <Adilfarq11@gmail.com>
2024-03-24 00:15:22 +0530

    URL query strings

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,31 +15,31 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func view(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet"))
-}
-
-func create(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet"))
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.Header().Set("Allow",http.MethodPost)
-		http.Error(w,"Method Not Allowed",http.StatusMethodNotAllowed)
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/snippetcreated", snippetCreate)
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", view)
-	mux.HandleFunc("/snippet/create", create)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Start server on :4000")
 	if err := http.ListenAndServe(":4000", mux); err != nil {
