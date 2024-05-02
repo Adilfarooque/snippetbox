e72245be6bb290a6b97d42f1cb2f88a274b862c7
Adilfarooque <Adilfarq11@gmail.com>
2024-05-02 20:55:04 +0530

    Updated

---
+++ HTML_template_composition/cmd/web/handlers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template" //New import
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	
+	tpl, err := template.ParseFiles("C:/Users/Adilf/OneDrive/Desktop/Code World/code/snippetbox./HTML_templating_inheritance/internal/ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error :", http.StatusInternalServerError)
+		return
+	}
+
+	if err = tpl.Execute(w, nil); err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d....", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
